67c944e572e6f127d567be4ef88f97d3c7b872d9
tonataher <154405713+tonataher@users.noreply.github.com>
2023-12-21 19:05:23 +0200

    Update Order.java

--- src/main/java/today/edu/Order.java
+++ src/main/java/today/edu/Order.java
@@ -1,13 +1,16 @@
 package today.edu;
 
 public class Order {
-    public String uname;
-    public String cname;
-    public String date;
+    private String uname;
+    private String cname;
+    private String date;
 
     public Order(String unamee, String cnamee, String datee){
         uname =  unamee;
         cname = cnamee;
         date = datee;
     }
+    public String getUname(){return uname;}
+    public String getCname(){return cname;}
+    public String getDate(){return date;}
 }
